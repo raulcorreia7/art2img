daf84b1f4549b39c4045bcd5d6205213aa53532a
Raúl Correia <19838093+raulcorreia7@users.noreply.github.com>
2025-10-06 03:17:30 +0200

    fix: print_version.cmake for release

--- cmake/print_version.cmake
+++ cmake/print_version.cmake
@@ -1,7 +1,7 @@
 cmake_minimum_required(VERSION 3.14)
 
 file(READ "${CMAKE_CURRENT_LIST_DIR}/../CMakeLists.txt" _root_cmakelists)
-string(REGEX MATCH "project\(\s*art2img\s+VERSION\s+([0-9]+\.[0-9]+\.[0-9]+)" _ "${_root_cmakelists}")
+string(REGEX MATCH "project\\([ \t\r\n]*art2img[ \t\r\n]+VERSION[ \t\r\n]+([0-9]+\\.[0-9]+\\.[0-9]+)" _ "${_root_cmakelists}")
 if(NOT CMAKE_MATCH_1)
   message(FATAL_ERROR "Unable to determine project version from CMakeLists.txt")
 endif()
